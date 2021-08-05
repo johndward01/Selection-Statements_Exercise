35dc272e1250143e09e088faac1c81699546869f
johndward01 <johndward01@gmail.com>
2021-08-05 16:28:28 -0500

    Add project files.

---
+++ Selection-Statements_Exericse-01-Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Selection_Statements_Exericse_01_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
