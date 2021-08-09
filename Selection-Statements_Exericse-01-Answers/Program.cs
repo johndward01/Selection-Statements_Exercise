670bd22f909569942735b05b35fd700ea4bdb90c
johndward01 <johndward01@gmail.com>
2021-08-09 15:59:12 -0500

    finished answer branch

--- Selection-Statements_Exericse-01-Answers/Program.cs
+++ Selection-Statements_Exericse-01-Answers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Selection_Statements_Exericse_01_Answers
 {
@@ -16,14 +17,16 @@ namespace Selection_Statements_Exericse_01_Answers
              * and inline-if statements 
             */
 
+
+
+            #region IF Statements
+
             // Use these variables for the IF & IF/ELSE statements
             int if_count = 0;
             int else_if_count = 0;
             int else_count = 0;
             bool var1 = true;
             bool var2 = false;
-
-            #region IF Statements
 
             Console.WriteLine("IF, IF/ELSE, IF/ELSE IF/ELSE Statements");
             Util.CreateTitleBreak();
@@ -205,6 +208,7 @@ namespace Selection_Statements_Exericse_01_Answers
 
             #endregion
 
+            #region IF/ELSE IF/ELSE Statements
 
             Console.WriteLine("IF/ELSE IF/ELSE Statements");
             Util.CreateSectionBreak();
@@ -263,12 +267,12 @@ namespace Selection_Statements_Exericse_01_Answers
 
             // TODO: Which statement gets run? The IF, ELSE IF, or ELSE statement?
             // TODO: What will be printed to the console?
-            if (y > 0 || z.Contains('c'))
+            if (!true && !false)
             {
                 if_count++;
                 Console.WriteLine($"if statement count: {if_count}");
             }
-            else if (x == 10)
+            else if (!true || !false)
             {
                 else_if_count++;
                 Console.WriteLine($"else if statement count: {else_if_count}");
@@ -302,8 +306,112 @@ namespace Selection_Statements_Exericse_01_Answers
             }
 
             Util.CreateStatementBreak();
+
+            #endregion
+
+            #region SWITCH/CASE Statements
+
+            // Use these variables for the SWITCH/CASE Statements below
+            var grades = new char[] { 'A', 'B', 'A', 'B', 'C', 'B', 'B', 'C', 'A', 'D', 'F', 'B'}; // A -> F
+            var orders = new int[] { 1, 4, 2, 3, 1, 2, 1, 3, 5, 1, 4, 4, 3 }; // 1 -> 5
+            var suits = new string[] { "Clubs", "Diamonds", "clubs", "hearts", "Spades", "diamonds", "Hearts", "spades" }; // clubs -> hearts
+
+            Console.WriteLine("SWITCH/CASE Statements");
+            Util.CreateTitleBreak();
+
+            // TODO: You can take in a user's input and replace the method with that instead for extra practice!
+
+            // TODO: Complete the switch/case statement below to print a response to a given grade ( use the grades variable and the ChooseRandomGrade() method)
+            switch (ChooseRandomGrade(grades))
+            {
+                case 'A':
+                    Console.WriteLine("You got an A!");
+                    break;
+                case 'B':
+                    Console.WriteLine("You got a B");
+                    break;
+                case 'C':
+                    Console.WriteLine("You got a C..");
+                    break;
+                case 'D':
+                    Console.WriteLine("You got a D...");
+                    break;
+                default:
+                    Console.WriteLine("You failed! :(");
+                    break;
+            }
+
+            Util.CreateStatementBreak();
+
+
+            // TODO: Create a switch/case to take a bunch of orders and print out the order to the console ( use the orders variable and the ChooseRandomOrder() method)
+            switch (ChooseRandomOrder(orders))
+            {
+                case 1:
+                    Console.WriteLine("A Cheeseburger, fries, and a drink.");
+                    break;
+                case 2:
+                    Console.WriteLine("A hotdog, fries, and a drink.");
+                    break;
+                case 3: Console.WriteLine("A salad, soup, and a drink.");
+                    break;
+                case 4:
+                    Console.WriteLine("Chicken tenders, fries, and a drink.");
+                    break;
+                case 5:
+                    Console.WriteLine("Cheese Ravioli, bread, and a drink.");
+                    break;
+                default:
+                    Console.WriteLine("Not on the menu.");
+                    break;
+            }
+
+            Util.CreateStatementBreak();
+
+
+            // TODO: Create a switch/case to Take a collection of suits (regardless of capitalization) and print the suit to the console
+            switch (ChooseRandomSuit(suits).ToLower())
+            {
+                case "clubs":
+                    Console.WriteLine("This is the suit of clubs.");
+                    break;
+                case "diamonds":
+                    Console.WriteLine("This is the suit of diamonds");
+                    break;
+                case "spades":
+                    Console.WriteLine("This is the suit of spades");
+                    break;
+                case "hearts":
+                    Console.WriteLine("This is the suit of hearts");
+                    break;
+                default:
+                    Console.WriteLine("This is not a suit of cards... is this a joker?");
+                    break;
+            }
+
+            Util.CreateStatementBreak();
+
+            #endregion
+
         }
 
-        
+        public static char ChooseRandomGrade(char[] grades)
+        {
+            var rng = new Random();
+            return grades[rng.Next(0, grades.Length - 1)];
+        }
+
+        public static int ChooseRandomOrder(int[] grades)
+        {
+            var rng = new Random();
+            return grades[rng.Next(0, grades.Length - 1)];
+        }
+
+        public static string ChooseRandomSuit(string[] suits)
+        {
+            var rng = new Random();
+            return suits[rng.Next(0, suits.Length - 1)];
+        }
+
     }
 }
