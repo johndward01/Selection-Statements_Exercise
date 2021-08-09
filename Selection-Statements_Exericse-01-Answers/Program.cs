d93800b212be9fa28bb5d0b72c564e4952b6f278
johndward01 <johndward01@gmail.com>
2021-08-09 11:42:30 -0500

    update

--- Selection-Statements_Exericse-01-Answers/Program.cs
+++ Selection-Statements_Exericse-01-Answers/Program.cs
@@ -16,12 +16,14 @@ namespace Selection_Statements_Exericse_01_Answers
              * and inline-if statements 
             */
 
-
+            // Use these variables for the IF & IF/ELSE statements
             int if_count = 0;
             int else_if_count = 0;
             int else_count = 0;
             bool var1 = true;
             bool var2 = false;
+
+            #region IF Statements
 
             Console.WriteLine("IF, IF/ELSE, IF/ELSE IF/ELSE Statements");
             Util.CreateTitleBreak();
@@ -88,7 +90,9 @@ namespace Selection_Statements_Exericse_01_Answers
 
             Util.CreateStatementBreak();
 
+            #endregion
 
+            #region IF/ELSE Statements
 
             // TODO: Call the CreateSectionBreak() function for the different selection statement sections
             Console.WriteLine("IF/ELSE Statements");
@@ -96,7 +100,25 @@ namespace Selection_Statements_Exericse_01_Answers
 
 
 
-            // TODO: Which statement gets run? The IF or ELSE IFstatement?
+            // TODO: Which statement gets run? The IF or ELSE statement?
+            // TODO: What will be printed to the console?
+            // TODO: Check your anwer by printing it to the console 
+            if (true)
+            {
+                Console.WriteLine($"if statement count: {if_count}");
+                if_count++;
+            }
+            else
+            {
+                Console.WriteLine($"else statement count: {else_count}");
+                else_count++;
+            }
+
+            Util.CreateStatementBreak();
+
+
+
+            // TODO: Which statement gets run? The IF or ELSE statement?
             // TODO: What will be printed to the console?
             // TODO: Check your anwer by printing it to the console 
             if (var2 == true)
@@ -114,7 +136,7 @@ namespace Selection_Statements_Exericse_01_Answers
 
 
 
-            // TODO: Which statement gets run? The IF or ELSE IFstatement?
+            // TODO: Which statement gets run? The IF or ELSE statement?
             // TODO: What will be printed to the console?
             // TODO: Check your anwer by printing it to the console
             if (var1 == false)
@@ -132,27 +154,9 @@ namespace Selection_Statements_Exericse_01_Answers
 
 
 
-            // TODO: Which statement gets run? The IF or ELSE IFstatement?
+            // TODO: Which statement gets run? The IF or ELSE statement?
             // TODO: What will be printed to the console?
             // TODO: Check your anwer by printing it to the console
-            if (var1 == false)
-            {
-                Console.WriteLine($"if statement count: {if_count}");
-                if_count++;
-            }
-            else
-            {
-                Console.WriteLine($"else statement count: {else_count}");
-                else_count++;
-            }
-
-            Util.CreateStatementBreak();
-
-
-
-            // TODO: Which statement gets run? The IF or ELSE IFstatement?
-            // TODO: What will be printed to the console?
-            // TODO: Check your anwer by printing the count to the console
             if (var2 == false)
             {
                 Console.WriteLine($"if statement count: {if_count}");
@@ -168,7 +172,25 @@ namespace Selection_Statements_Exericse_01_Answers
 
 
 
-            // TODO: Which statement gets run? The IF or ELSE IFstatement?
+            // TODO: Which statement gets run? The IF or ELSE statement?
+            // TODO: What will be printed to the console?
+            // TODO: Check your anwer by printing the count to the console
+            if (var2 != var1)
+            {
+                Console.WriteLine($"if statement count: {if_count}");
+                if_count++;
+            }
+            else
+            {
+                Console.WriteLine($"else statement count: {else_count}");
+                else_count++;
+            }
+
+            Util.CreateStatementBreak();
+
+
+
+            // TODO: Which statement gets run? The IF or ELSE statement?
             // TODO: What will be printed to the console?
             if (if_count == else_count)
             {
@@ -181,28 +203,105 @@ namespace Selection_Statements_Exericse_01_Answers
 
             Util.CreateStatementBreak();
 
+            #endregion
 
 
             Console.WriteLine("IF/ELSE IF/ELSE Statements");
             Util.CreateSectionBreak();
 
 
-            
-            // Use these variables for the next set of statements
+            // Use these variables for the IF / ELSE IF / ELSE statements
             int x = 10;
             double y = -.0221;
             string z = "abc";
-            if_count = 0;
-            else_count = 0;
-            else_if_count = 0;
+            if_count = 0; // resetting the value to 0 (re-initializing the variable to 0)
+            else_count = 0; // reset
+            else_if_count = 0; // reset
+
+
 
             // TODO: Which statement gets run? The IF, ELSE IF, or ELSE statement?
             // TODO: What will be printed to the console?
             if (x == 0)
             {
-                Console.WriteLine($"if statement count: {if_count}");
+                Console.WriteLine($"x equals 0");
+            }
+            else if (x > 0)
+            {
+                Console.WriteLine($"x is greater than 0");
+            }
+            else
+            {
+                Console.WriteLine($"x is less than 0");
             }
 
+            Util.CreateStatementBreak();
+
+
+
+            // TODO: Which statement gets run? The IF, ELSE IF, or ELSE statement?
+            // TODO: What will be printed to the console?
+            if (y > 0 || z.Contains('c'))
+            {
+                if_count++;
+                Console.WriteLine($"if statement count: {if_count}");
+            }
+            else if (x == 10)
+            {
+                else_if_count++;
+                Console.WriteLine($"else if statement count: {else_if_count}");
+            }
+            else
+            {
+                else_count++;
+                Console.WriteLine($"else statement count: {else_count}");
+            }
+
+            Util.CreateStatementBreak();
+
+
+
+            // TODO: Which statement gets run? The IF, ELSE IF, or ELSE statement?
+            // TODO: What will be printed to the console?
+            if (y > 0 || z.Contains('c'))
+            {
+                if_count++;
+                Console.WriteLine($"if statement count: {if_count}");
+            }
+            else if (x == 10)
+            {
+                else_if_count++;
+                Console.WriteLine($"else if statement count: {else_if_count}");
+            }
+            else
+            {
+                else_count++;
+                Console.WriteLine($"else statement count: {else_count}");
+            }
+
+            Util.CreateStatementBreak();
+
+
+
+            // TODO: Which statement gets run? The IF, ELSE IF, or ELSE statement?
+            // TODO: What will be printed to the console?
+            if (if_count > else_if_count && if_count > else_count)
+            {
+                if_count++;
+                Console.WriteLine($"if statement count: {if_count}");
+            }
+            else if (else_if_count > if_count && else_if_count > else_count)
+            {
+                else_if_count++;
+                Console.WriteLine($"else if statement count: {else_if_count}");
+            }
+            else
+            {
+                else_count++;
+                Console.WriteLine($"else statement count: {else_count}");
+            }
+
+            Util.CreateStatementBreak();
         }
 
         
